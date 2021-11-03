9a93379fdbb861585a6c480ad66b31c62af1d3a4
Jim MacLeod <jmacleod@gmail.com>
2021-11-03 15:36:00 -0700

    if auth refresh fails, re-auth

--- spam-sift.py
+++ spam-sift.py
@@ -28,6 +28,7 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+import google.auth.exceptions
 
 from httplib2 import Http
 from oauth2client import file, client, tools
@@ -258,16 +259,19 @@ creds = None
 if os.path.exists('token.json'):
     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 # If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
+if creds and creds.expired and creds.refresh_token:
+    try:
         creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('token.json', 'w') as token:
-        token.write(creds.to_json())
+    except google.auth.exceptions.RefreshError as error:
+        print('Refresh error: %s' % error)
+        creds = None
+if not creds or not creds.valid:
+    flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+    creds = flow.run_local_server(port=0)
+# Save the credentials for the next run
+with open('token.json', 'w') as token:
+    token.write(creds.to_json())
 
 service = build('gmail', 'v1', credentials=creds)
 
